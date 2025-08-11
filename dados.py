d5891334248593e5722a62c893a1841a87ae7d7d
Jonasdeproenca <jonaadeprienca@gmail.com>
2025-08-10 21:39:52 -0300

    Adiciona script para entrada e saída de dados

---
+++ dados.py
@@ -0,0 +1,5 @@
+nome = input("Qual o seu nome? ")
+idade = int(input("Qual a sua idade? "))
+altura = float(input("Qual a sua altura em metros? "))
+
+print(f"Olá {nome}, você tem {idade} anos e mede {altura} metros.")
